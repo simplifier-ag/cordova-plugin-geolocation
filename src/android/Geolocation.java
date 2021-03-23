ef8332f75b14fcb0204d483b888f7e7e5a30bbc0
Ponsen <paulstresow@gmail.com>
2021-03-23 16:27:07 +0100

    [Android] - added GPS_USAGE_DESCRIPTION preference - cleaned imports - added usage description dialog (rejects request if closed without confirming, requests permission otherwise) - added resolver for Ids and string resources

--- src/android/Geolocation.java
+++ src/android/Geolocation.java
@@ -18,30 +18,30 @@
 
 package org.apache.cordova.geolocation;
 
-import android.content.pm.PackageManager;
 import android.Manifest;
-import android.os.Build;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.view.Window;
 
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
 import org.apache.cordova.PermissionHelper;
 import org.apache.cordova.PluginResult;
-import org.apache.cordova.LOG;
 import org.json.JSONArray;
-import org.json.JSONException;
-
-import javax.security.auth.callback.Callback;
 
 public class Geolocation extends CordovaPlugin {
 
     String TAG = "GeolocationPlugin";
     CallbackContext context;
 
+    AlertDialog featureDescriptionDialog = null;
+
     String [] permissions = { Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION };
 
-
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
         LOG.d(TAG, "We are entering execute");
         context = callbackContext;
         if(action.equals("getPermission"))
@@ -53,17 +53,45 @@ public class Geolocation extends CordovaPlugin {
                 return true;
             }
             else {
-                PermissionHelper.requestPermissions(this, 0, permissions);
+                showUsageDescription(localize(
+                        cordova.getContext(),
+                        "gps_usage_description"
+                ));
             }
             return true;
         }
         return false;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions,
-                                          int[] grantResults) throws JSONException
-    {
+    private void showUsageDescription(String message) {
+        featureDescriptionDialog = new AlertDialog.Builder(cordova.getContext())
+                .setMessage(message)
+                .setPositiveButton(cordova.getContext().getResources().getText(android.R.string.ok),
+                        (dialog, which) -> {
+                    featureDescriptionDialog.setOnDismissListener(null);
+                    dialog.dismiss();
+                    PermissionHelper.requestPermissions(this, 0, permissions);
+                })
+                .setOnDismissListener(dialog -> {
+                    //dialog got dismissed/back pressed
+                    PluginResult result;
+                    if(context != null) {
+                        LOG.d(TAG, "Permission Denied!");
+                        result = new PluginResult(PluginResult.Status.ILLEGAL_ACCESS_EXCEPTION);
+                        context.sendPluginResult(result);
+                    }
+                })
+                .create();
+        featureDescriptionDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        featureDescriptionDialog.show();
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
         PluginResult result;
         //This is important if we're using Cordova without using Cordova, but we have the geolocation plugin installed
         if(context != null) {
@@ -102,6 +130,31 @@ public class Geolocation extends CordovaPlugin {
         PermissionHelper.requestPermissions(this, requestCode, permissions);
     }
 
+    /**
+     * @param filename     Name of the file
+     * @param resourceType Type of resource (ID, STRING, LAYOUT, DRAWABLE)
+     * @return The associated resource identifier. Returns 0 if no such resource was found. (0 is not a valid resource ID.)
+     */
+    private int getResourceId(Context context, String filename, String resourceType) {
+        String package_name = context.getPackageName();
+        Resources resources = context.getResources();
 
+        return resources.getIdentifier(filename, resourceType, package_name);
+    }
 
+    /**
+     * @param identifier string identifier of the resource
+     * @return The localized string. Returns identifier if no such resource was found.
+     */
+    public String localize(Context context, String identifier, Object... args) {
+        int id = getResourceId(context, identifier, "string");
+
+        if (id == 0) {
+            return identifier;
+        } else {
+            return args.length > 0
+                    ? String.format(context.getResources().getString(id), args)
+                    : context.getResources().getString(id);
+        }
+    }
 }
